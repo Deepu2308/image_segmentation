213afbb649c3a7942f8776c97fa3cd21151e43a3
deepu <deeputvm1992@gmail.com>
2021-01-10 03:21:15 +0530

    changes tesnor shape to BCHW

--- src/dataset.py
+++ src/dataset.py
@@ -18,21 +18,24 @@ class DataLoaderSegmentation(data.Dataset):
     refer https://discuss.pytorch.org/t/dataloader-for-semantic-segmentation/48290
     """
     
-    def __init__(self, folder_path):
+    def __init__(self, 
+                 file_list
+                 ):
         super(DataLoaderSegmentation, self).__init__()
         
         #save path variable
-        self.path = folder_path
+        self.path = os.getcwd() + '/input/'
         
         #get filenames
-        self.img_files  = os.listdir(self.path + '/png_images')    
         self.img_files  = [self.path + r'/png_images/' + i \
-                           for i in self.img_files]
+                           for i in file_list]
         
         #function to get mask name
         self.mask_map  = lambda x:  x.replace('images', 'masks') 
         
-            
+        #used to converto to numpy and axes order ( change BHWC to BCHW )
+        #self.reorder_axes = lambda x: np.moveaxis(x, [0,1,2,3], [0,2,3,1])
+        
     def __getitem__(self, index):
         
             #get filenames for batch
@@ -44,8 +47,14 @@ class DataLoaderSegmentation(data.Dataset):
             mask   = Image.open(mask_path)
             
             #convert to RGB and then numpy array
-            image,mask = np.array(image.convert('RGB')),\
-                                np.array(mask.convert('RGB'))
+            #image,mask = image.convert('RGB'),mask.convert('RGB')
+
+            #add channel dimension
+            image,mask = np.expand_dims(np.array(image),0),\
+                                np.expand_dims(np.array(mask),0)
+            
+            #reorder axes
+            #image,mask =  self.reorder_axes(image), self.reorder_axes()
             
             #return 
             return torch.from_numpy(image).float().cuda(), torch.from_numpy(mask).float().cuda()
@@ -55,10 +64,13 @@ class DataLoaderSegmentation(data.Dataset):
     
 if __name__ == '__main__'    :
     
+    
+    file_list = os.listdir(r"C:\Users\deepu\Desktop\MyFolders\Projects\PetProjects\image_segmentation/input/png_images") 
+    
     #sample loader
-    ld = DataLoaderSegmentation(r"C:\Users\deepu\Desktop\MyFolders\Projects\PetProjects\image_segmentation\input")
+    ld = DataLoaderSegmentation(file_list)
 
     #get sample
     img, msk = ld.__getitem__(0)
 
-    img.shape 
+    print(img.shape)
